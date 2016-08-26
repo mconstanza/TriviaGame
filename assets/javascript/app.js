ccb7b4d1f171970d59cd17e0a039682ec8e8cf51
Michael Constanza <michael.constanza@gmail.com>
2016-08-26 19:10:58 -0400

    added next question function which checks the current questionIndex against the length of the questions array and ends the game when appropriate.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,10 @@
 
 $(document).ready(function(){
 
-
+// Declare Variable ////////////////////////////////////////////////////////////////////////////
+	
+	// Dom Elements /////////////////////////////
+	var $timerDiv = $('#timerDiv');
 	var $timeRemaining = $('#timeRemaining');
 	var $question = $('#questionDiv');
 	var $choice = $('.choice');
@@ -11,10 +14,19 @@ $(document).ready(function(){
 	var $choice2 = $('#choice2');
 	var $choice3 = $('#choice3');
 	var $choice4 = $('#choice4');
+	var $start = $('#startButton');
 
+	// Question transitioning ///////////////////
 	var questions = [];
+	var questionIndex = 0;
+
+	// Game variables ///////////////////////////
+	var wins = 0;
+	var losses = 0;
+	var questionAnswered = false;
 
 
+// Question Object ////////////////////////////////////////////////////////////////////////////
 	function question(text, choices, answer, background, answerMedia){
 
 		this.text = text;
@@ -23,7 +35,9 @@ $(document).ready(function(){
 		this.background = background;
 		this.answerMedia = answerMedia;
 
-		this.displayChoices = function(){
+		this.displayQuestion = function(){
+
+			$question.html(this.text);
 
 			// write the answer choice to the array, then set a data attribute = to the choice string
 			$choice1.html(this.choices[0]);
@@ -41,27 +55,67 @@ $(document).ready(function(){
 
 	}
 
+// Question Creation //////////////////////////////////////////////////////////////////////////
 	var test = new question('What is my name?', ['Steve', 'Jack', 'Mike', 'Blaine'],
 		'Mike')
 
-	currentQuestion = test;
+	questions.push(test);
 
-	$question.html(currentQuestion.text);
-	currentQuestion.displayChoices();
+	currentQuestion = questions[questionIndex];
 
 
+// General Functions //////////////////////////////////////////////////////////////////////////
+	function nextQuestion(){
+
+		if (questionIndex == questions.length) {
+			// end game
+			$question.html("Game Over!")
+
+		}else {
+
+			currentQuestion = questions[questionIndex]
+			currentQuestion.displayQuestion();
+			questionAnswered = false;
+		}
+
+	}
+
+// OnClick functions //////////////////////////////////////////////////////////////////////////
+
+	// when start button is clicked ///////////////////////////////////////////////////////////
+	$start.on('click', function(){
+		$timerDiv.html("Time Remaining: <span id='timeRemaining'></span>")
+		nextQuestion();
+
+	})
+	
+	// when an answer choice is clicked, compare it to the correct answer
 	$choice.on('click', function(){
 
+		if (questionAnswered == false){
+
+			questionAnswered = true;
+		
+		
 		console.log('answer: ' + currentQuestion.answer)
 		console.log('choice: ' + $(this).data('choice'))
 
-		if($(this).data('choice') == currentQuestion.answer ){
-			// right answer logic
-			console.log("Right answer!")
-		}else {
-			// wrong answer logic
-			console.log("Wrong answer!")
-		}	
+			if($(this).data('choice') == currentQuestion.answer){
+				// right answer logic
+				console.log("Right answer!")
+				$question.html("Correct!")
+				questionIndex += 1;
+
+				setTimeout(nextQuestion, 3000);
+			}else {
+				// wrong answer logic
+				console.log("Wrong answer!")
+				$question.html("Wrong!")
+				questionIndex += 1;
+
+				setTimeout(nextQuestion, 3000);
+			}	
+		}
 	})
 
 
