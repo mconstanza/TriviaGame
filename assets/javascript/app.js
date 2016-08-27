de064a13fcef0cde256dcfd9173fc004b9c09ef7
Michael Constanza <michael.constanza@gmail.com>
2016-08-27 16:34:38 -0400

    Added lots of Batman-themed questions and set up logic to shuffle the questions each time the game is played.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -66,10 +66,7 @@ $(document).ready(function(){
 	stopTimer = function(){
 		clearInterval(counter);
 		$timeRemaining.empty();
-		
-
-
-		
+		time = 10;
 	}
 
 
@@ -115,13 +112,61 @@ $(document).ready(function(){
 		$choice2.html("Incorrect: " + losses);
 	}
 
+	function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
 // Question Creation //////////////////////////////////////////////////////////////////////////
-	var test = new question('What is my name?', ['Steve', 'Jack', 'Mike', 'Blaine'],
-		'Mike');
+	// var test = new question('What is my name?', ['Steve', 'Jack', 'Mike', 'Blaine'],
+	// 	'Mike');
 
-	var test2 = new question("what color are my eyes?", ['blue', 'brown', 'green', 'purple'], 'blue');
+	// var test2 = new question("what color are my eyes?", ['blue', 'brown', 'green', 'purple'], 'blue');
 
-	questions = [test, test2];
+	var movieReleased = new question("When was the first Batman movie released?", ['1966', '1943', 
+		'1968', '1989'], '1943');
+
+	var penguinName = new question("What is the Penguin's real name?", ['Oswald Cobblepot',
+		'Edward Nygma', 'Norman Osborn', 'John Wayne Corben'], 'Oswald Cobblepot');
+
+	var backBreak = new question("What villain once broke Batman's back, leaving him crippled?",
+		["Ra's Al Ghul", 'Bane', 'Killer Croc', 'Mr. Freeze'], 'Bane');
+
+	var batmanVoice = new question("Who voiced Batman in 7 different cartoon series?",
+		['Kevin Conroy', 'Timothy Daly', 'Nolan North', 'Christian Bale'], "Kevin Conroy");
+
+	var jokerVoice = new question("Who voiced the Joker in Batman: The Animated Series?",
+		['Kevin Michael Richardson', 'Ceasar Romero', 'Jack Nicholson', 'Mark Hamill'], 
+		'Mark Hamill');
+
+	var killingJoke = new question("Who got shot during 'The Killing Joke' storyline?",
+		['Robin', 'Alfred', 'Barbara Gordon', 'Batman'], 'Barbara Gordon');
+
+	var quinnMeet = new question("Where did Harley Quinn meet the Joker?",
+		['Gotham General', 'Arkham Asylum', 'Gotham police station', 'Bellereave'], "Arkham Asylum");
+
+	var barbOracle = new question("Which of these was one of Barbara Gordon's aliases?",
+		['B', 'The Watcher', 'Oracle', 'Huntress'], 'Oracle');
+
+	var nightWing = new question("What was Dick Grayson's new alias after he stopped being Robin?",
+		[ 'Red Hood', 'Nightwing', 'Moonknight', 'The Question'], 'Nightwing');
+
+	var robinKilled = new question("Which Robin was killed by the Joker?", ['Dick Grayson', 'Jason Todd', 'Tim Drake', 'Damian Wayne'],
+		'Jason Todd');
+
+	var waynesKilled = new question("Who killed Bruce Wayne's parents?", ['The Joker', 'The Riddler', 'Joe Chill', 'Victor Zsaz'],
+		'Joe Chill');
+
+
+	questions = [ movieReleased, penguinName, backBreak, batmanVoice, jokerVoice, killingJoke, quinnMeet,
+	 barbOracle, nightWing, robinKilled, waynesKilled];
+
+	questions = shuffleArray(questions);
 
 	currentQuestion = questions[questionIndex];
 
